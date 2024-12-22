edb96880af2c48f5d213e77884b02ff47cb2c197
brunoferromega <brunoferromega@outlook.com>
2024-12-22 10:20:38 -0300

    change/nvim: remove trigger completions for rust lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,87 +1,98 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
-		{ "j-hui/fidget.nvim" },
-		{ "folke/lazydev.nvim" },
-	},
-	config = function()
-		require("mason").setup()
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    { "j-hui/fidget.nvim" },
+    { "folke/lazydev.nvim" },
+  },
+  config = function()
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"rust_analyzer",
-				"clangd",
-				"csharp_ls",
-				"gopls",
-				"jdtls",
-				"ts_ls",
-				"pylsp",
-			},
-			automatic_installation = false,
-		})
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "clangd",
+        "csharp_ls",
+        "gopls",
+        "jdtls",
+        "ts_ls",
+        "pylsp",
+      },
+      automatic_installation = false,
+    })
 
-		require("cmp").setup({
-			completion = { autocomplete = false },
-		})
+    require("cmp").setup({
+      completion = { autocomplete = false },
+    })
 
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.snippetSupport = false
 
     local disable_diagnostics = function(_, _)
       vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
     end
 
-		local lspconfig = require("lspconfig")
+    local lspconfig = require("lspconfig")
 
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = { "vim" },
-					},
-				},
-			},
-			capabilities = capabilities,
-		})
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { "vim" },
+          },
+        },
+      },
+      capabilities = capabilities,
+    })
 
-		lspconfig.rust_analyzer.setup({
+    lspconfig.rust_analyzer.setup({
       on_attach = function(client, bufrn)
+        client.server_capabilities.completionProvider = false
         disable_diagnostics(client, bufrn)
       end,
       capabibilites = capabilities,
-		})
+    })
 
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.gopls.setup({
+      on_attach = function(client, bufrn)
+        disable_diagnostics(client, bufrn)
+      end,
+      capabilities = capabilities,
+    })
 
-		lspconfig.csharp_ls.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.gopls.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.clangd.setup({
+      on_attach = function(client, bufrn)
+        disable_diagnostics(client, bufrn)
+      end,
+      capabilities = capabilities,
+    })
 
-		lspconfig.pylsp.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.csharp_ls.setup({
+      on_attach = function(client, bufrn)
+        disable_diagnostics(client, bufrn)
+      end,
+      capabilities = capabilities,
+    })
 
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.jdtls.setup({
+      capabilities = capabilities,
+    })
 
-		lspconfig.jdtls.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.pylsp.setup({
+      capabilities = capabilities,
+    })
 
-		lspconfig.vimls.setup({
-			capabilities = capabilities,
-		})
-	end,
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.vimls.setup({
+      capabilities = capabilities,
+    })
+  end,
 }
